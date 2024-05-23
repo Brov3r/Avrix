e1a3fb77913c014a39cf736879d49e1ebe35e1cf
Deknil <147500212+Brov3r@users.noreply.github.com>
2024-05-23 13:01:41 +0300

    Update AvrixLauncher.bat

--- scripts/AvrixLauncher.bat
+++ scripts/AvrixLauncher.bat
@@ -1,40 +1,40 @@
 @echo off
 setlocal enabledelayedexpansion
 
+echo ======================= Avrix Launcher =======================
+
+rem Determine the OS bitness
+:determineOS
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set "osArchitecture=x86" || set "osArchitecture=x64"
+
+echo [Avrix-Launcher] Current OS: %osArchitecture%
+
 rem Prompt the user to choose the Steam version
 :chooseSteam
 set /p steamVersion="[Avrix-Launcher] Use Steam version? (0 - no, 1 - yes): "
 if "%steamVersion%"=="1" (
     set "steamFlag=1"
+    set "useSteamText=yes"
 ) else if "%steamVersion%"=="0" (
     set "steamFlag=0"
+    set "useSteamText=no"
 ) else (
     echo [Avrix-Launcher] Invalid input. Please enter 0 or 1.
     goto chooseSteam
 )
 
-rem Prompt the user to choose the OS version
-:chooseOS
-set /p os="[Avrix-Launcher] Use x86 or x64 OS version? (0 - x86, 1 - x64): "
-if "%os%"=="1" (
+rem Set the appropriate variables based on the OS bitness
+set "classPath=java/;java/istack-commons-runtime.jar;java/jassimp.jar;java/javacord-2.0.17-shaded.jar;java/javax.activation-api.jar;java/jaxb-api.jar;java/jaxb-runtime.jar;java/lwjgl.jar;java/lwjgl-glfw.jar;java/lwjgl-jemalloc.jar;java/lwjgl-opengl.jar;java/lwjgl_util.jar;java/sqlite-jdbc-3.27.2.1.jar;java/trove-3.0.3.jar;java/uncommons-maths-1.2.3.jar;java/commons-compress-1.18.jar;"
+
+if "%osArchitecture%"=="x64" (
     set "javaExec=.\jre64\bin\java.exe"
     set "libraryPath=natives/;natives/win64/;."
     set "javaOptions=-XX:+UseZGC -Xms16g -Xmx16g"
-) else if "%os%"=="0" (
+    set "classPath=!classPath!java/lwjgl-natives-windows.jar;java/lwjgl-glfw-natives-windows.jar;java/lwjgl-jemalloc-natives-windows.jar;java/lwjgl-opengl-natives-windows.jar;"
+) else (
     set "javaExec=.\jre\bin\java.exe"
     set "libraryPath=natives/;natives/win32/;./"
     set "javaOptions=-XX:+UseG1GC -Xms768m -Xmx768m"
-) else (
-    echo [Avrix-Launcher] Invalid input. Please enter 0 or 1.
-    goto chooseOS
-)
-
-rem Initialize the classPath variable based on user input
-set "classPath=java/;java/istack-commons-runtime.jar;java/jassimp.jar;java/javacord-2.0.17-shaded.jar;java/javax.activation-api.jar;java/jaxb-api.jar;java/jaxb-runtime.jar;java/lwjgl.jar;java/lwjgl-glfw.jar;java/lwjgl-jemalloc.jar;java/lwjgl-opengl.jar;java/lwjgl_util.jar;java/sqlite-jdbc-3.27.2.1.jar;java/trove-3.0.3.jar;java/uncommons-maths-1.2.3.jar;java/commons-compress-1.18.jar;"
-
-if "%os%"=="1" (
-    set "classPath=!classPath!java/lwjgl-natives-windows.jar;java/lwjgl-glfw-natives-windows.jar;java/lwjgl-jemalloc-natives-windows.jar;java/lwjgl-opengl-natives-windows.jar;"
-) else (
     set "classPath=!classPath!java/lwjgl-natives-windows-x86.jar;java/lwjgl-glfw-natives-windows-x86.jar;java/lwjgl-jemalloc-natives-windows-x86.jar;java/lwjgl-opengl-natives-windows-x86.jar;"
 )
 
@@ -48,7 +48,9 @@ set "javaArg=-Djava.awt.headless=true -Dzomboid.steam=%steamFlag% -Dzomboid.znet
 
 rem Launch the Java application
 
-echo [Avrix-Launcher] Starting the server with %jarFile%..
+echo [Avrix-Launcher] Core: %jarFile% ^| OS: %osArchitecture% ^| Steam version: %useSteamText%
+echo [Avrix-Launcher] Loading a server...
+
 %javaExec% %javaArg% com.avrix.Main %1 %2
 
 pause
