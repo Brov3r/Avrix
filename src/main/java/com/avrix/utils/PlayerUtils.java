9a096cf7f377e90935dd13e4913532efc3f46054
Brov3r <147500212+Brov3r@users.noreply.github.com>
2024-06-12 23:36:12 +0300

    Update PlayerUtils.java

--- src/main/java/com/avrix/utils/PlayerUtils.java
+++ src/main/java/com/avrix/utils/PlayerUtils.java
@@ -52,7 +52,7 @@ public class PlayerUtils {
      * @return Player's SteamID or null if Steam mode is disabled or SteamID not found.
      */
     public static String getPlayerSteamID(IsoPlayer player) {
-        if (SteamUtils.isSteamModeEnabled()) return null;
+        if (!SteamUtils.isSteamModeEnabled()) return null;
 
         return String.valueOf(player.getSteamID());
     }
